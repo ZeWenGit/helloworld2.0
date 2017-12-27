bf7d90dbcce62f0e7aebde6405542ee61eff70c7
wuzewen <zewen.wu@foxmail.com>
2017-12-27 09:21:25 +0800

    Initial commit

---
+++ src/test/java/com/springmvc/dao/UserMapperTest.java
@@ -0,0 +1,28 @@
+package com.springmvc.dao;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by zewen.wu on 2017/12/26.
+ */
+public class UserMapperTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void insert() throws Exception {
+
+    }
+
+}
